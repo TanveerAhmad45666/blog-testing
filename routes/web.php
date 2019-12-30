b7e755efcbd2250e41157901e6e802ec5016332c
TanveerAhmad45666 <55710635+TanveerAhmad45666@users.noreply.github.com>
2019-12-29 19:28:22 -0800

    its second commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/admin/post', function () {
+//    return view('welcome');
+    return "admin is here";
 });
+Route::get('/contact',function (){
+    return "its contact page";
+});
+Route::get('/post/{id}',function($id){
+     return"its working" . $id;
+});
+Route::get('/post/{id}/{name}',function($id,$name){
+    return"its parmater" .$id." " .$name;
+});
+Route::get('/admin/post/example',array('as'=>'admin.home',function(){
+    $url = route ('admin.home');
+    return "this url is" .$url;
+}));
