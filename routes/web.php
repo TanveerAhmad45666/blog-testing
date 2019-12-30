870e3e92d790408f374bf0d78d3efe33b84b20f6
TanveerAhmad45666 <55710635+TanveerAhmad45666@users.noreply.github.com>
2019-12-29 21:33:40 -0800

    its third commit
    yes its third commit

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,24 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/admin/post', function () {
-//    return view('welcome');
-    return "admin is here";
-});
-Route::get('/contact',function (){
-    return "its contact page";
-});
-Route::get('/post/{id}',function($id){
-     return"its working" . $id;
-});
-Route::get('/post/{id}/{name}',function($id,$name){
-    return"its parmater" .$id." " .$name;
-});
-Route::get('/admin/post/example',array('as'=>'admin.home',function(){
-    $url = route ('admin.home');
-    return "this url is" .$url;
-}));
+//Route::get('/admin/post', function () {
+////    return view('welcome');
+//    return "admin is here";
+//});
+//Route::get('/contact',function (){
+//    return "its contact page";
+//});
+//Route::get('/post/{id}',function($id){
+//     return"its working" . $id;
+//});
+//Route::get('/post/{id}/{name}',function($id,$name){
+//    return"its parmater" .$id." " .$name;
+//});
+//Route::get('/admin/post/example',array('as'=>'admin.home',function(){
+//    $url = route ('admin.home');
+//    return "this url is" .$url;
+//}));
+//Route::get('/posts/{id}','PostController@index');
+Route::resource('posts','PostController');
+
+
